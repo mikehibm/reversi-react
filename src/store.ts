324ed3201e5d28fd5a496dcaace555cc2b307da2
Makoto Ishida <makotoishida@gmail.com>
2018-11-02 15:36:00 -1000

    storeクラスのリファクタリング。状態を変更する際にコピーを作成する。

--- src/store.ts
+++ src/store.ts
@@ -42,81 +42,56 @@ export interface AppState {
 }
 
 class Store extends EventEmitter {
-  page: Pages = 'menu';
-  board: BoardState = this.initBoard();
+  state: AppState = {
+    page: 'menu',
+    board: this.initBoard(),
+  };
 
   getState(): AppState {
-    const names = ['', 'Black', 'White'];
-    const playerName = names[this.board.turn];
-    const winnerName = this.board.finished ? names[this.getWinner(this.board)] : '';
-
-    return {
-      page: this.page,
-      board: {
-        cells: cloneCells(this.board.cells),
-        turn: this.board.turn,
-        turnCount: this.board.turnCount,
-        blackCount: this.board.blackCount,
-        whiteCount: this.board.whiteCount,
-        placeableCount: this.board.placeableCount,
-        finished: this.board.finished,
-        currentPlayerName: playerName,
-        winnerName: winnerName,
-      },
-    };
-
-    function cloneCells(cells: CellState[][]): CellState[][] {
-      return cells.map((row, rowIndex) => {
-        return row.map((cell, colIndex) => ({
-          index: cell.index,
-          row: cell.row,
-          col: cell.col,
-          color: cell.color,
-          placeable: cell.placeable,
-          turnableCells: [...cell.turnableCells],
-        }));
-      });
-    }
+    return this.state;
   }
 
   setPage(page: Pages): void {
-    this.page = page;
-    console.log(`page=${page}`);
-
     // Initialize the board before going back to game screen.
-    if (page === 'game') {
-      this.board = this.initBoard();
-    }
+    const board = page === 'game' ? this.initBoard() : this.state.board;
 
+    this.state = { ...this.getState(), page, board };
     this.emit('page_changed');
   }
 
   setStone({ row, col }: CellClick): void {
-    const cell = this.board.cells[row][col];
+    const board = this.state.board;
+
+    const cell = board.cells[row][col];
     if (!cell.placeable) {
       return;
     }
 
     // Place a stone.
-    cell.color = this.board.turn;
+    cell.color = board.turn;
 
     // Turn all the stones that are in the middle.
-    cell.turnableCells.forEach((target) => {
-      target.color = cell.color;
+    cell.turnableCells.forEach(({ row, col }) => {
+      board.cells[row][col].color = cell.color;
     });
 
-    this.changeTurn();
-  }
-
-  changeTurn(): void {
-    this.board.turnCount++;
-    this.board.turn = this.board.turn === Colors.Black ? Colors.White : Colors.Black;
-    this.updateCellStatus(this.board);
-    this.emit('board_changed');
+    this.changeTurn(board);
   }
 
   skipTurn(): void {
-    this.changeTurn();
+    this.changeTurn(this.state.board);
+  }
+
+  changeTurn(board: BoardState): void {
+    const newBoard = {
+      ...board,
+      cells: this.cloneCells(board.cells),
+    };
+    newBoard.turnCount++;
+    newBoard.turn = newBoard.turn === Colors.Black ? Colors.White : Colors.Black;
+    this.updateBoardStatus(newBoard);
+    this.state = { ...this.getState(), board: newBoard };
+    this.emit('board_changed');
   }
 
   initBoard(): BoardState {
@@ -149,78 +124,84 @@ class Store extends EventEmitter {
       finished: false,
     };
 
-    this.updateCellStatus(board);
+    this.updateBoardStatus(board);
+    return board;
+  }
+
+  cloneCells(cells: CellState[][]): CellState[][] {
+    return cells.map((row, rowIndex) => {
+      return row.map((cell, colIndex) => ({ ...cell }));
+    });
+  }
+
+  updateBoardStatus(board: BoardState) {
+    let countWhite = 0,
+      countBlack = 0,
+      countNone = 0,
+      countPlaceable = 0;
+
+    board.cells.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        cell.placeable = this.checkIfPlaceable(board, cell, board.turn);
+        if (cell.placeable) countPlaceable++;
+        if (cell.color === Colors.White) countWhite++;
+        if (cell.color === Colors.Black) countBlack++;
+        if (cell.color === Colors.None) countNone++;
+      });
+    });
+
+    board.whiteCount = countWhite;
+    board.blackCount = countBlack;
+    board.finished = countNone === 0 || countWhite === 0 || countBlack === 0;
+    board.placeableCount = countPlaceable;
+
+    const names = ['', 'Black', 'White'];
+    const playerName = names[board.turn];
+    const winnerName = board.finished ? names[this.getWinner(board)] : '';
+    board.currentPlayerName = playerName;
+    board.winnerName = winnerName;
 
     return board;
   }
 
-  updateCellStatus(board: BoardState) {
-    const cells = board.cells;
-    cells.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        cell.placeable = checkIfPlaceable(cell, board.turn);
-      });
-    });
-
-    board.blackCount = this.countColor(board, Colors.Black);
-    board.whiteCount = this.countColor(board, Colors.White);
-    board.finished = this.countColor(board, Colors.None) === 0;
-    board.placeableCount = this.getPlaceableCount(board);
-
-    function checkIfPlaceable(cell: CellState, turn: Colors): boolean {
-      cell.turnableCells = [];
-      if (cell.color !== Colors.None) return false;
-      for (let dy = -1; dy <= 1; dy++) {
-        for (let dx = -1; dx <= 1; dx++) {
-          const foundCells = searchTurnableCells(dx, dy, cell, turn, []);
-          cell.turnableCells = cell.turnableCells.concat(foundCells);
-        }
+  checkIfPlaceable(board: BoardState, cell: CellState, turn: Colors): boolean {
+    cell.turnableCells = [];
+    if (cell.color !== Colors.None) return false;
+    for (let dy = -1; dy <= 1; dy++) {
+      for (let dx = -1; dx <= 1; dx++) {
+        const foundCells = this.searchTurnableCells(board, dx, dy, cell, turn, []);
+        cell.turnableCells = cell.turnableCells.concat(foundCells);
       }
-      return cell.turnableCells.length > 0;
     }
-
-    function searchTurnableCells(dx: number, dy: number, cell: CellState, turn: Colors, arr: CellState[]): CellState[] {
-      if (dx === 0 && dy === 0) return [];
-      const r = cell.row + dy;
-      const c = cell.col + dx;
-      if (r < 0 || c < 0 || r > ROWS - 1 || c > COLS - 1) return [];
-      const opponentColor = turn === Colors.Black ? Colors.White : Colors.Black;
-      const nextCell = cells[r][c];
-
-      if (nextCell.color === opponentColor) {
-        arr.push(nextCell);
-        return searchTurnableCells(dx, dy, nextCell, turn, arr);
-      } else if (nextCell.color === turn) {
-        return arr;
-      }
-      return [];
-    }
+    return cell.turnableCells.length > 0;
   }
 
-  getPlaceableCount(board: BoardState): number {
-    let count = 0;
-    const cells = board.cells;
-    cells.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        if (cell.placeable) count++;
-      });
-    });
-    return count;
-  }
+  searchTurnableCells(
+    board: BoardState,
+    dx: number,
+    dy: number,
+    cell: CellState,
+    turn: Colors,
+    arr: CellState[]
+  ): CellState[] {
+    if (dx === 0 && dy === 0) return [];
+    const r = cell.row + dy;
+    const c = cell.col + dx;
+    if (r < 0 || c < 0 || r > ROWS - 1 || c > COLS - 1) return [];
+    const opponentColor = turn === Colors.Black ? Colors.White : Colors.Black;
+    const nextCell = board.cells[r][c];
 
-  countColor(board: BoardState, color: Colors): number {
-    let count = 0;
-    const cells = board.cells;
-    cells.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        if (cell.color === color) count++;
-      });
-    });
-    return count;
+    if (nextCell.color === opponentColor) {
+      arr.push(nextCell);
+      return this.searchTurnableCells(board, dx, dy, nextCell, turn, arr);
+    } else if (nextCell.color === turn) {
+      return arr;
+    }
+    return [];
   }
 
   getWinner(board: BoardState): Colors {
-    const { blackCount, whiteCount } = this.board;
+    const { blackCount, whiteCount } = board;
     return blackCount === whiteCount ? Colors.None : blackCount > whiteCount ? Colors.Black : Colors.White;
   }
 }
